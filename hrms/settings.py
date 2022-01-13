b9065d549d7309bd8489a5908c814c5e89e5ae80
Sans Sankhe <sanssankhe@mail.com>
2022-01-13 15:30:07 +0530

    second commit

--- hrms/settings.py
+++ hrms/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL="/Employee"
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
